7bb204ce7f890674056c6b05f024e32bd2025b39
David Carter <indysoftwaredev@gmail.com>
2025-02-12 20:00:15 -0500

    Add EF Core and Identity setup for database access
    Updated `Angular-Core-Auth.Server.csproj` to include
    new package references for Entity Framework Core and
    ASP.NET Core Identity. Enhanced `Program.cs` to
    configure authentication, authorization, and database
    context using SQL Server. Modified
    `appsettings.Development.json` to add a connection
    string for local database access.

--- Angular-Core-Auth.Server/Program.cs
+++ Angular-Core-Auth.Server/Program.cs
@@ -1,3 +1,8 @@
+using Angular_Core_Auth.Server.Database;
+using Angular_Core_Auth.Server.Extensions;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,18 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddAuthorization();
+builder.Services.AddAuthentication().AddCookie(IdentityConstants.ApplicationScheme)
+    .AddBearerToken(IdentityConstants.BearerScheme);
+builder.Services.AddIdentityCore<User>()
+    .AddEntityFrameworkStores<ApplicationDbContext>()
+    .AddApiEndpoints();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
+});
 
 var app = builder.Build();
 
@@ -17,9 +34,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+
+    app.ApplyMigrations();
 }
 
 app.UseHttpsRedirection();
+
+app.MapIdentityApi<User>();
 
 app.UseAuthorization();
 
